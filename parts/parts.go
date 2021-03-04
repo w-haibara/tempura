4f45e56b58b6bd048703369f0aa62a578d18934c
w-haibara <hwhaibarawataru@gmail.com>
2021-03-05 01:46:33 +0900

    refactor

--- parts/parts.go
+++ parts/parts.go
@@ -35,7 +35,6 @@ func Header2() string {
 </head>
 <body>
 <script>`
-//	str += CrossDomain()
 	str += "$('body').terminal(function(command, term) {\n"
 	return str
 }
@@ -97,53 +96,6 @@ func Json(p []config.Prompt) string {
 	}
 	str += `'}'`
 	return str
-}
-
-func CrossDomain() string {
-	return `
-(function( jQuery ) {
-	if ( window.XDomainRequest ) {
-		jQuery.ajaxTransport(function( s ) {
-			if ( s.crossDomain && s.async ) {
-				if ( s.timeout ) {
-					s.xdrTimeout = s.timeout;
-					delete s.timeout;
-					}
-				var xdr;
-				return {
-					send: function( _, complete ) {
-						function callback( status, statusText, responses, responseHeaders ) {
-							xdr.onload = xdr.onerror = xdr.ontimeout = jQuery.noop;
-							xdr = undefined;
-							complete( status, statusText, responses, responseHeaders );
-							}
-						xdr = new XDomainRequest();
-						xdr.onload = function() {
-							callback( 200, "OK", { text: xdr.responseText }, "Content-Type: " + xdr.contentType );
-						};
-						xdr.onerror = function() {
-								callback( 404, "Not Found" );
-						};
-						xdr.onprogress = jQuery.noop;
-							xdr.ontimeout = function() {
-							callback( 0, "timeout" );
-						};
-						xdr.timeout = s.xdrTimeout || Number.MAX_VALUE;
-						xdr.open( s.type, s.url );
-						xdr.send( ( s.hasContent && s.data ) || null );
-					},
-					abort: function() {
-						if ( xdr ) {
-							xdr.onerror = jQuery.noop;
-							xdr.abort();
-						}
-					}
-				};
-			}
-		});
-	}
-})( jQuery );
-`
 }
 
 func Api(a config.Api, pop bool) string {
